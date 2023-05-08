31e9e9721dd97779d75d6a0f2385dee49f2c6e78
Wojtek Adamczyk <wojtek.adamczyk99@gmail.com>
2023-05-08 18:13:55 +0200

    Initial commit

---
+++ src/main/java/todoapp/Todoapp/model/TaskRepository.java
@@ -0,0 +1,11 @@
+package todoapp.Todoapp.model;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TaskRepository extends JpaRepository<Task, Integer>
+{
+
+}
