441d3613a0660d2c38f3b99dca76523b7cb0f9db
Wojtek Adamczyk <wojtek.adamczyk99@gmail.com>
2023-05-09 19:45:37 +0200

    Dodanie walidacji do desc

--- src/main/java/todoapp/Todoapp/model/Task.java
+++ src/main/java/todoapp/Todoapp/model/Task.java
@@ -1,15 +1,18 @@
 package todoapp.Todoapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
 public class Task
 {
+    Task() {}
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Tasks description must not be null or empty")
     private String description;
     private boolean done;
 
@@ -18,7 +21,7 @@ public class Task
         return id;
     }
 
-    public void setId(int id) {
+    void setId(int id) {
         this.id = id;
     }
 
@@ -26,7 +29,7 @@ public class Task
         return description;
     }
 
-    public void setDescription(String description) {
+    void setDescription(String description) {
         this.description = description;
     }
 
@@ -34,7 +37,7 @@ public class Task
         return done;
     }
 
-    public void setDone(boolean done) {
+    void setDone(boolean done) {
         this.done = done;
     }
 }
--- src/main/java/todoapp/Todoapp/model/TaskRepository.java
+++ src/main/java/todoapp/Todoapp/model/TaskRepository.java
@@ -1,6 +1,5 @@
 package todoapp.Todoapp.model;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
