2ee473b54880241fc226c1ab60ab9a567fcaa018
Wojtek Adamczyk <wojtek.adamczyk99@gmail.com>
2023-05-15 15:40:26 +0200

    validator nie działa nie wiem czemu

--- src/main/java/todoapp/Todoapp/TodoAppApplication.java
+++ src/main/java/todoapp/Todoapp/TodoAppApplication.java
@@ -3,22 +3,18 @@ package todoapp.Todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
-public class TodoAppApplication implements RepositoryRestConfigurer
+public class TodoAppApplication
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
-
-
 	@Bean
-	Validator validator()
+	LocalValidatorFactoryBean validator()
 	{
 		return new LocalValidatorFactoryBean();
 	}
